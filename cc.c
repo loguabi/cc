c358d3bac46209fe9fe9a2aab92b031cc558d6b2
logitha <loga848.456@gmail.com>
2016-07-02 10:31:37 +0530

    Create cc.c

---
+++ cc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char ch[20]={2,7,11,15),;
+int a;
+printf("Enter the target value:");
+scanf("%c",&a);
+str(ch[0]+ch[1]);
+a=ch[0]+ch[1];
+printf("Enter the target :%d",a);
+return 0;
+}
+
